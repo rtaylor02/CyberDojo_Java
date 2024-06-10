86685287947905d1ec1d20f80d8ab77a0432ac9c
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2024-06-10 11:50:34 +0100

    Set up as maven project

---
+++ src/test/java/com/rtaylor02/AppTest.java
@@ -0,0 +1,12 @@
+package com.rtaylor02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertEquals("potato", "potato");
+    }
+}
