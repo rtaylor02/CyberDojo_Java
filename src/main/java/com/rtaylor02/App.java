d720152108a4058eda0004fb550edd9613858f18
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2024-06-13 16:22:20 +0100

    Add my own solution

--- src/main/java/com/rtaylor02/App.java
+++ src/main/java/com/rtaylor02/App.java
@@ -1,13 +1,39 @@
 package com.rtaylor02;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    private List<Character> list1 = new ArrayList<>(List.of('B', 'S', 'D', 'C', 'N', 'G', 'R', 'T', 'Q', 'F', 'J', 'H', 'V', 'A', 'O', 'E', 'F', 'L', 'P', 'Z'));
+    private List<Character> list2 = new ArrayList<>(List.of('O', 'K', 'Q', 'P', 'A', 'T', 'E', 'G', 'D', 'S', 'W', 'U', 'I', 'N', 'B', 'R', 'S', 'Y', 'C', 'M'));
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean canMakeWord(String word) {
+        char[] chars = word.toCharArray();
+
+        int found = 0; // Flag if each character from the word is found in the lists
+        for (char aChar : chars) {
+            // Check list 1 first
+            int index = list1.indexOf(aChar);
+
+            // Check list2 if list1 does not have the letter
+            if (index == -1) {
+                index = list2.indexOf(aChar);
+            }
+
+            // Remove found index from both list
+            if (index >= 0) {
+                list1.remove(index);
+                list2.remove(index);
+                found++;
+            } else {
+                break;
+            }
+        }
+
+        return found == chars.length;
     }
 }
--- src/test/java/com/rtaylor02/AppTest.java
+++ src/test/java/com/rtaylor02/AppTest.java
@@ -1,12 +1,106 @@
 package com.rtaylor02;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
+    App systemUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        systemUnderTest = new App();
+    }
+
+    // GIVEN a word "A"
+    // WHEN it is checked using canMakeWord() method
+    // THEN it should return true as it is possible to make the word
     @Test
-    public void shouldAnswerWithTrue() {
-        assertEquals("potato", "potato");
+    void shouldReturnTrue_WhenAIsPassed() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+        assertTrue(systemUnderTest.canMakeWord("A"));
+    }
+
+    // GIVEN a word "BARK"
+    // WHEN it is checked using canMakeWord() method
+    // THEN it should return true as it is possible to make the word
+    @Test
+    void shouldReturnTrue_WhenBARKIsPassed() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+        assertTrue(systemUnderTest.canMakeWord("BARK"));
+    }
+
+    // GIVEN a word "BOOK"
+    // WHEN it is checked using canMakeWord() method
+    // THEN it should return false as it is possible to make the word
+    @Test
+    void shouldReturnFalse_WhenBOOKIsPassed() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+        assertFalse(systemUnderTest.canMakeWord("BOOK"));
+    }
+
+    // GIVEN a word "TREAT"
+    // WHEN it is checked using canMakeWord() method
+    // THEN it should return true as it is possible to make the word
+    @Test
+    void shouldReturnTrue_WhenTREATIsPassed() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+        assertTrue(systemUnderTest.canMakeWord("TREAT"));
+    }
+
+    // GIVEN a word "COMMON"
+    // WHEN it is checked using canMakeWord() method
+    // THEN it should return false as it is possible to make the word
+    @Test
+    void shouldReturnFalse_WhenCOMMONIsPassed() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+        assertFalse(systemUnderTest.canMakeWord("COMMON"));
+    }
+
+    // GIVEN a word "SQUAD"
+    // WHEN it is checked using canMakeWord() method
+    // THEN it should return true as it is possible to make the word
+    @Test
+    void shouldReturnTrue_WhenSQUADIsPassed() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+        assertTrue(systemUnderTest.canMakeWord("SQUAD"));
+    }
+
+    // GIVEN a word "CONFUSE"
+    // WHEN it is checked using canMakeWord() method
+    // THEN it should return true as it is possible to make the word
+    @Test
+    void shouldReturnTrue_WhenCONFUSEIsPassed() {
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+        assertTrue(systemUnderTest.canMakeWord("CONFUSE"));
     }
 }
